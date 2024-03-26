1e931502247b3439a9f4fbf02ee783cd480e5a52
ClintonCode20 <clintonmatics@gmail.com>
2024-03-26 11:06:07 +0100

    added pg

--- personalized_learning/settings.py
+++ personalized_learning/settings.py
@@ -116,12 +116,25 @@ WSGI_APPLICATION = 'personalized_learning.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ztebzkpb',
+        'USER': 'ztebzkpb',
+        'PASSWORD': '5oK7jHparJ6aQ2-8aKHwFkMcbx2_G8Hf',
+        'HOST': 'cornelius.db.elephantsql.com', 
+        'PORT': '5432',
+    }  
 }
+
+
 
 
 # Password validation
